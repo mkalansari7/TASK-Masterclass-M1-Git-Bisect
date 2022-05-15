ec54975f12bb6c40ffe734f15c135b1f9ca348e1
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-05-15 14:02:03 +0300

    Colorize output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import typer
 
 def greet(name: str = typer.Option(..., prompt=True)) -> None:
     """Welcome to our awesome CLI app!"""
-    typer.echo(f"Hello, {name}!")
+    typer.secho(f"Hello, {name}!", fg=typer.colors.GREEN)
 
 
 if __name__ == "__main__":

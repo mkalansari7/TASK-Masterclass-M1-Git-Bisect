8c887ffb7dd51b1c8f1a8dd8796991295ea3b769
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-05-15 14:03:06 +0300

    Emphasize name

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import typer
 
 def greet(name: str = typer.Option(..., prompt=True)) -> None:
     """Welcome to our awesome CLI app!"""
-    typer.secho(f"Hello, {name}!", fg=typer.colors.GREEN)
+    bolded = typer.style(name, bold=True)
+    typer.secho(f"Hello, {bolded}!", fg=typer.colors.GREEN)
 
 
 if __name__ == "__main__":
